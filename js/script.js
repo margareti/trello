cf0e8728839644b8b646ae2de62325386c5499e0
Margarita Ignatyeva <margaret.vegan@gmail.com>
2016-10-23 22:21:28 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+class Column() {
+	constructor() {
+
+	}
+}
+
+const test = [
+ {
+ 	name: 'Red-Black Trees',
+ 	description: 'Решить задачу про черно-красные деревья',
+ 	order: 1,
+ 	status: 'todo'
+ },
+ {
+ 	name: 'AVL Trees',
+ 	description: 'Решить задачу про АВЛ деревья',
+ 	order: 2,
+ 	status: 'todo'
+ },
+ {
+ 	name: 'ESlint',
+ 	description: 'Figure out how to set $PATH variables on mac'
+ 	order: 0,
+ 	status: 'inprogress'
+ },
+ {
+ 	name: 'SublimeLinter',
+ 	description: 'install SublimeLinter',
+ 	order: 1,
+ 	status: 'done'
+ }
+]
+
