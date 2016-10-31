1b45aa7150e17fddc053c6f7c1814aaa0d24c4e3
Margarita Ignatyeva <margaret.vegan@gmail.com>
2016-10-31 23:42:48 +0200

    syntax eslint

--- js/script.js
+++ js/script.js
@@ -1,152 +1,128 @@
 class Column {
-	constructor(arr, idx) {
-		this.list = [];
-		for (let item in arr) {
-			this.list.push(arr[item]);
-		}
+  constructor(arr, idx) {
+    this.list = [];
+    for (let item in arr) {
+      this.list.push(arr[item]);
+    }
 
-		console.log(this.list)
-		this.list = this.list.sort(function(a, b) {
-			return a.order - b.order;
-		})
-		this.index = idx;
-		this.map = ['To do', 'In progress', 'Done'];
-	}
-	render() {
-		const div = document.createElement('div');
-		const title = document.createElement('h2');
+    this.list = this.list.sort((a, b) => {
+      return a.order - b.order;
+    });
+    this.index = idx;
+    this.map = ['To do', 'In progress', 'Done'];
+  }
+  render() {
+    const div = document.createElement('div');
+    const title = document.createElement('h2');
+    title.textContent = this.map[this.index];
 
-		
-	  title.textContent = this.map[this.index];
-
-		const list = document.createElement('ul');
-		for (let el = 0; el < this.list.length; el ++) {
-			let first = false;
-		  let last = false;
-
-			if (el === 0) {
-				first = true;
-			} 
-			if (el === this.list.length - 1) {
-				last = true;
-			}
-			list.appendChild(this.list[el].render(first, last));
-		}
-		div.appendChild(title);
-		div.appendChild(list);
-		return div;
-	}
+    const list = document.createElement('ul');
+    for (let el = 0; el < this.list.length; el ++) {
+      let first = false;
+      let last = false;
+      if (el === 0) {
+        first = true;
+      }
+      if (el === this.list.length - 1) {
+        last = true;
+      }
+      list.appendChild(this.list[el].render(first, last));
+    }
+    div.appendChild(title);
+    div.appendChild(list);
+    return div;
+  }
 }
 
 class Board {
-	constructor(arr, node) {
+  constructor(arr, node) {
+    this.node = document.querySelector(node);
 
-		this.node = document.querySelector(node);
+    this.list = [];
+    arr.forEach((el) => {
+      this.list.push(new Task(el));
+    });
+    this.map = {
+      todo: 0,
+      inprogress: 1,
+      done: 2,
+    };
 
-		this.list = [];
-		arr.forEach((el) => {
-			console.log(new Task(el));
-			this.list.push(new Task(el));
-		});
+    this.node.addEventListener('removeTask', (e) => {
+      const index = this.list.indexOf(e.detail);
+      this.list.splice(index, 1);
+      this.render();
+    });
 
+    this.node.addEventListener('moveUp', (e) => {
+      e.detail.order -= 2;
+      this.render();
+    });
 
-		this.map = {
-			'todo': 0,
-			'inprogress': 1,
-			'done': 2,
-		};
+    this.node.addEventListener('moveDown', (e) => {
+      e.detail.order += 2;
+      this.render();
+    });
 
-		this.node.addEventListener('removeTask', (e) => {
-	   console.log(e.detail);
-	   const index = this.list.indexOf(e.detail);
-	   this.list.splice(index, 1);
-	   this.render();
-	  });
+    this.node.addEventListener('moveLeft', (e) => {
+      if (e.detail.status === 'done') {
+        e.detail.status = 'inprogress';
+      } else if (e.detail.status === 'inprogress') {
+        e.detail.status = 'todo';
+      }
+      e.detail.order = this.getNewOrder(e.detail);
+      this.render();
+    });
 
-	  this.node.addEventListener('moveUp', (e) => {
-	  	const index = this.list.indexOf(e.detail);
-	    e.detail.order -= 2;
-	    this.render();
-	  });
+    this.node.addEventListener('moveRight', (e) => {
+      if (e.detail.status === 'todo') {
+        e.detail.status = 'inprogress';
+      } else if (e.detail.status === 'inprogress') {
+        e.detail.status = 'done';
+      }
+      e.detail.order = this.getNewOrder(e.detail);
+      this.render();
+    });
 
-	  this.node.addEventListener('moveDown', (e) => {
-	  	const index = this.list.indexOf(e.detail);
-	    e.detail.order += 2;
-	    this.render();
-	  });
+    this.node.addEventListener('addEvent', (e) => {
+      this.list.push(new Task(e.detail));
+      this.render();
+    });
+    this.render();
+  }
 
-	  this.node.addEventListener('moveLeft', (e) => {
-	  	const index = this.list.indexOf(e.detail);
-	  	if (e.detail.status === 'done') {
-	  		e.detail.status = 'inprogress';
+  render() {
+    this.columns = [[], [], []];
+    this.node.innerHTML = '';
 
-	  	} else if (e.detail.status === 'inprogress'){
-	  		e.detail.status = 'todo';
-	  	}
+    this.list.forEach((el) => {
+      this.columns[this.map[el.status]].push(el);
+    });
 
-	  	e.detail.order =  this.getNewOrder(e.detail);
-	    this.render();	
-	  });
+    const taskKeys = Object.keys(this.columns);
+    taskKeys.forEach((column, idx) => {
+      const taskList = new Column(this.columns[column], idx);
+      taskList.render();
+      this.node.appendChild(taskList.render());
+    });
+    this.node.appendChild(this.addForm());
+  }
 
-
-
-	  this.node.addEventListener('moveRight', (e) => {
-	  	const index = this.list.indexOf(e.detail);
-	  	console.log(e.detail)
-	  	if (e.detail.status === 'todo') {
-	  		e.detail.status = 'inprogress';
-
-	  	} else if (e.detail.status === 'inprogress'){
-	  		e.detail.status = 'done';
-	  	}
-	  	
-	  	e.detail.order = this.getNewOrder(e.detail);
-	    this.render();
-	  });
-
-	  this.node.addEventListener('addEvent', (e) => {
-	  	this.list.push(new Task(e.detail));
-	  	this.render();
-	  })
-
-	  this.render();
-	}
-
-	render() {
-		this.columns = [[], [], []];
-		this.node.innerHTML = '';
-
-		this.list.forEach((el) => {
-			console.log(el);
-			this.columns[this.map[el.status]].push(el);
-		})
-
-		const taskKeys = Object.keys(this.columns);
-		taskKeys.forEach((column, idx) => {
-
-			const taskList = new Column(this.columns[column], idx)
-			taskList.render()
-			this.node.appendChild(taskList.render());
-
-		})
-		this.node.appendChild(this.addForm());
-	}
-
-	getNewOrder(el) {
-		const columnLength = this.columns[this.map[el.status]].length;
-  	let lastElOrder;
-  	if (columnLength > 0) {
-  		this.columns[this.map[el.status]].sort((a, b) => {
-  			return a.order - b.order;
-  		})
-  		lastElOrder = this.columns[this.map[el.status]][columnLength - 1].order;
-  		lastElOrder += 1;
-  	} else {
-  		lastElOrder = 0;
-  	}
-  	return lastElOrder;
-	}
-	createInput(type, name, required, inputClass, placeholder) {
+  getNewOrder(el) {
+    const columnLength = this.columns[this.map[el.status]].length;
+    let lastElOrder;
+    if (columnLength > 0) {
+      this.columns[this.map[el.status]].sort((a, b) => {
+        return a.order - b.order;
+      });
+      lastElOrder = this.columns[this.map[el.status]][columnLength - 1].order;
+      lastElOrder += 1;
+    } else {
+      lastElOrder = 0;
+    }
+    return lastElOrder;
+  }
+  createInput(type, name, required, inputClass, placeholder) {
     const input = document.createElement('input');
     input.type = type;
     input.name = name;
@@ -157,9 +133,9 @@ class Board {
   }
   addForm() {
     const status = {
-      'todo': 'To Do',
-      'inprogress': 'In Progress',
-      'done': 'Done',
+      todo: 'To Do',
+      inprogress: 'In Progress',
+      done: 'Done',
     };
 
     const wrap = document.createElement('div');
@@ -199,26 +175,22 @@ class Board {
       select.appendChild(option);
     });
     select.value = 'todo';
-    let newTaskData = {};
+    const newTaskData = {};
 
-  	const addEvent = new CustomEvent('addEvent', {
-    	bubbles: true,
-    	detail: newTaskData,
-    })
+    const addEvent = new CustomEvent('addEvent', {
+      bubbles: true,
+      detail: newTaskData,
+    });
 
-  
-    submit.addEventListener('click', function () {
-    	newTaskData.name = title.value;
-    	newTaskData.description = textarea.value;
-    	newTaskData.order = parseInt(numberInput.value);
-    	newTaskData.status = select.value;
-
-    	console.log(newTaskData);
-    	if (newTaskData.name) {
-    		console.log(newTaskData);
-    		this.dispatchEvent(addEvent);
-    	}
-    })
+    submit.addEventListener('click', function submitForm() {
+      newTaskData.name = title.value;
+      newTaskData.description = textarea.value;
+      newTaskData.order = parseInt(numberInput.value, 10);
+      newTaskData.status = select.value;
+      if (newTaskData.name) {
+        this.dispatchEvent(addEvent);
+      }
+    });
 
     fieldset1.appendChild(label1);
     fieldset1.appendChild(select);
@@ -237,128 +209,124 @@ class Board {
 }
 
 class Task {
-	constructor(obj) {
-		this.name = obj.name;
-		this.description = obj.description;
-		this.order = obj.order;
-		this.status = obj.status;
-	}
-	render(first, last) {
-		const task = document.createElement('li');
-		const title = document.createElement('h3');
-		const desc = document.createElement('p');
-		const close = document.createElement('span');
-		const left = document.createElement('span');
-		const right = document.createElement('span');
-		const top = document.createElement('span');
-		const bottom = document.createElement('span');
+  constructor(obj) {
+    this.name = obj.name;
+    this.description = obj.description;
+    this.order = obj.order;
+    this.status = obj.status;
+  }
+  render(first, last) {
+    const task = document.createElement('li');
+    const title = document.createElement('h3');
+    const desc = document.createElement('p');
+    const close = document.createElement('span');
+    const left = document.createElement('span');
+    const right = document.createElement('span');
+    const top = document.createElement('span');
+    const bottom = document.createElement('span');
 
-		if (!first) {
-			top.textContent = '›';
-			top.classList.add('control--top');
-			top.classList.add('control');
-			task.appendChild(top);
-		}
+    if (!first) {
+      top.textContent = '›';
+      top.classList.add('control--top');
+      top.classList.add('control');
+      task.appendChild(top);
+    }
 
-		if (this.status !== 'todo') {
-			left.textContent = '‹';
-			left.classList.add('control--left');
-			left.classList.add('control');
-			task.appendChild(left);
-		}
+    if (this.status !== 'todo') {
+      left.textContent = '‹';
+      left.classList.add('control--left');
+      left.classList.add('control');
+      task.appendChild(left);
+    }
 
-		if (this.status !== 'done') {
-			right.textContent = '›';
-			right.classList.add('control--right');
-			right.classList.add('control');
-			task.appendChild(right);
-		}
+    if (this.status !== 'done') {
+      right.textContent = '›';
+      right.classList.add('control--right');
+      right.classList.add('control');
+      task.appendChild(right);
+    }
 
-		if (!last) {
-			bottom.textContent = '›';
-			bottom.classList.add('control--bottom');
-			bottom.classList.add('control');
-			task.appendChild(bottom);
-		}
-
-
-		close.textContent = '×';
-		close.classList.add('close');
-
-		title.textContent = this.name;
-		desc.textContent = this.description;
-
-		task.appendChild(close);
-		task.appendChild(title);
-		task.appendChild(desc);
+    if (!last) {
+      bottom.textContent = '›';
+      bottom.classList.add('control--bottom');
+      bottom.classList.add('control');
+      task.appendChild(bottom);
+    }
 
 
-		const remove = new CustomEvent('removeTask', {
-	   bubbles: true,
-	   detail: this
-	  });
-	  const moveUp = new CustomEvent('moveUp', {
-	  	bubbles: true,
-	  	detail: this
-	  })
-	  const moveDown = new CustomEvent('moveDown', {
-	  	bubbles: true,
-	  	detail: this
-	  })
-	  const moveRight = new CustomEvent('moveRight', {
-	  	bubbles: true,
-	  	detail: this
-	  })
+    close.textContent = '×';
+    close.classList.add('close');
 
-	  const moveLeft = new CustomEvent('moveLeft', {
-	  	bubbles: true,
-	  	detail: this
-	  })
+    title.textContent = this.name;
+    desc.textContent = this.description;
 
-	  close.addEventListener('click', function() {
-	   this.dispatchEvent(remove);
-	  });
-
-	  top.addEventListener('click', function() {
-	  	this.dispatchEvent(moveUp);
-	  })
-
-	  right.addEventListener('click', function() {
-	  	this.dispatchEvent(moveRight);
-	  })
-
-	  left.addEventListener('click', function() {
-	  	this.dispatchEvent(moveLeft);
-	  })
-
-	  bottom.addEventListener('click', function() {
-	  	this.dispatchEvent(moveDown);
-	  })
+    task.appendChild(close);
+    task.appendChild(title);
+    task.appendChild(desc);
 
 
-		return task;
-	}
+    const remove = new CustomEvent('removeTask', {
+      bubbles: true,
+      detail: this,
+    });
+    const moveUp = new CustomEvent('moveUp', {
+      bubbles: true,
+      detail: this,
+    });
+    const moveDown = new CustomEvent('moveDown', {
+      bubbles: true,
+      detail: this,
+    });
+    const moveRight = new CustomEvent('moveRight', {
+      bubbles: true,
+      detail: this,
+    });
+
+    const moveLeft = new CustomEvent('moveLeft', {
+      bubbles: true,
+      detail: this,
+    });
+
+    close.addEventListener('click', function toRemove() {
+      this.dispatchEvent(remove);
+    });
+
+    top.addEventListener('click', function up() {
+      this.dispatchEvent(moveUp);
+    });
+
+    right.addEventListener('click', function toRight() {
+      this.dispatchEvent(moveRight);
+    });
+
+    left.addEventListener('click', function toLeft() {
+      this.dispatchEvent(moveLeft);
+    });
+
+    bottom.addEventListener('click', function down() {
+      this.dispatchEvent(moveDown);
+    });
+    return task;
+  }
 }
 
-const test = [
- {
- 	name: 'Red-Black Trees',
- 	description: 'Решить задачу про черно-красные деревья',
- 	order: 0,
- 	status: 'todo'
- },
- {
- 	name: 'AVL Trees',
- 	description: 'Решить задачу про АВЛ деревья',
- 	order: 1,
- 	status: 'todo'
- },
- {
- 	name: 'ESlint',
- 	description: 'Figure out how to set $PATH variables on mac',
- 	order: 0,
- 	status: 'inprogress'
- },
+const test = [{
+  name: 'Red-Black Trees',
+  description: 'Решить задачу про черно-красные деревья',
+  order: 0,
+  status: 'todo' },
+  {
+    name: 'AVL Trees',
+    description: 'Решить задачу про АВЛ деревья',
+    order: 1,
+    status: 'todo',
+  },
+  {
+    name: 'ESlint',
+    description: 'Figure out how to set $PATH variables on mac',
+    order: 0,
+    status: 'inprogress',
+  },
  //  {
  // 	name: '5.1',
  // 	description: 'Finish the assignment',
@@ -371,17 +339,10 @@ const test = [
  // 	order: 2,
  // 	status: 'inprogress'
  // },
- {
- 	name: 'SublimeLinter',
- 	description: 'install SublimeLinter',
- 	order: 0,
- 	status: 'done'
- }
-]
-
-n = new Board(test, '.board');
-console.log(n);
-
-
-
-
+  {
+    name: 'SublimeLinter',
+    description: 'install SublimeLinter',
+    order: 0,
+    status: 'done',
+  },
+];
