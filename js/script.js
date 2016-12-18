1ef620345d860bfe98b8bff6c4d5aebd9408ee6f
Margarita Ignatyeva <margaret.vegan@gmail.com>
2016-12-19 00:01:27 +0200

    feat: 3 columns

--- js/script.js
+++ js/script.js
@@ -1,26 +1,44 @@
+const app = {};
+app.board = [];
+
 const Task = Backbone.Model.extend({
   defaults: {
-    name: 'New Task Title',
-    description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+    name: 'Sample Title',
+    description: 'Sample description',
   },
 });
 
 const TaskView = Backbone.View.extend({
   tagName: 'li',
-  template:$('#list-item').html(),
-  render: function() {
-    var tmpl = _.template(this.template);
-    this.$el.html(tmpl(this.model.toJSON()));
+  template:_.template($('#list-item').html()),
+  initialize: function(options) {
+    this.model = options;
+    this.render();
+  },
+  render: function(item) {
+    this.$el.html(this.template({name: this.model.name, description: this.model.description}));
     return this;
   },
 });
+const Column = Backbone.Collection.extend({
+  model: Task,
+});
 
 const ColumnView = Backbone.View.extend({
-  tagName: 'div',
   template: _.template($('#list-column').html()),
-  render: function(title) {
-    const tmp = this.template({title: title})
+  initialize: function(options) {
+    this.model = options;
+    
+  },
+  render: function() {
+    const tmp = this.template({title: this.model.title});
     this.$el.html(tmp);
+    this.$list = this.$('ul');
+    this.model.tasks.forEach(x => {
+      console.log("list ", this.$list)
+      var task = new TaskView(x);
+      this.$list.append(task.render().el);
+    });
     return this;
   },
 });
@@ -29,44 +47,51 @@ const Board = Backbone.Collection.extend({
   model: Task,
 });
 
+const columns = {
+  todo: {
+    title: 'To Do',
+    id: 0,
+  },
+  inprogress: {
+    title: 'In Progress',
+    id: 1,
+  },
+  done: {
+    title: 'Done',
+    id: 2,
+  },
+};
+
 const BoardView = Backbone.View.extend({
   el: '.board',
-  // template: _.template($('#list-item').html()),
-  initialize: function() {
+  initialize: function(options) {
+    this.model = options;
     this.render();
   },
   render: function() {
-    this.$el.html('');
-    const columns = [{
-      title: 'To Do',
-      id: 0,
-    }, {
-      title: 'In progress',
-      id: 1,
-    }, {
-      title: 'Done',
-      id: 2,
-    }];
+    this.model.forEach(x => {
+      if (!columns[x.status].tasks) {
+        columns[x.status].tasks = [];
+      }
+      columns[x.status].tasks.push(x);
+    });
 
-    columns.forEach(x => {
-      const column = new ColumnView();
-      this.$el.append(column.render(x.title).el);
-      console.log(column.render(x.title).el);
-    })
-    
+    var columnKeys = Object.keys(columns);
+    columnKeys.forEach(x => {
+      var column = new ColumnView(columns[x]);
+      this.$el.append(column.render().el);
+    });
   },
 });
 
-
-
-const test = [{
+const tasks = [{
   name: 'Red-Black Trees',
-  description: 'Решить задачу про черно-красные деревья',
+  description: 'Do red-back trees assignment',
   order: 0,
   status: 'todo' },
   {
     name: 'AVL Trees',
-    description: 'Решить задачу про АВЛ деревья',
+    description: 'Also do the AVL Trees algorithm task',
     order: 1,
     status: 'todo',
   },
@@ -83,8 +108,5 @@ const test = [{
     status: 'done',
   },
 ];
-// const board = new Board(test);
-// const view = new BoardView();
-// view.render();
-// const task = new TaskView();
-const view = new BoardView();
+
+var board = new BoardView(tasks);
